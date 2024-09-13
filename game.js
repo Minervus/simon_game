b92ec459158f8dbf0178ba77d8f122c295106573
Tony Nguyen <tnguyen411@gmail.com>
2024-09-13 13:59:05 -0700

    added click listeners and sounds

--- game.js
+++ game.js
@@ -1,12 +1,47 @@
+var userClickedPattern = [];
 var gamePattern = [];
-
 var buttonColours = ["red", "blue", "green", "yellow"];
+var randomChosenColour = buttonColours[nextSequence()]; 
 
-var randomChosenColour = buttonColours[randomNumber]; 
-
-gamePattern.append(randomChosenColour);
+gamePattern.push(randomChosenColour);
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
-    console.log(randomNumber);
+    return randomNumber; 
+}
+
+$("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100); 
+
+
+
+$('div[type="button"]').on("click",function() {
+    var userChosenColour = $(this).attr("id"); 
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+
+
+});
+
+function playSound(soundColour) {
+    switch(soundColour) {
+        case "red":
+            var redSound = new Audio('sounds/red.mp3');
+            redSound.play();
+            break;
+        case "blue":
+            var blueSound = new Audio('sounds/blue.mp3');
+            blueSound.play(); 
+            break;
+        case "green":
+            var greenSound = new Audio('sounds/green.mp3');
+            greenSound.play(); 
+            break;
+        case "yellow":
+            var yellowSound = new Audio('sounds/yellow.mp3');
+            yellowSound.play(); 
+            break;
+        default:
+            console.log(randomChosenColour);
+    }
+
 }
